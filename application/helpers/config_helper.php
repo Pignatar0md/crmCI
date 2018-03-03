2848268e1b22d3a8915003ba0dc6cac3c116f220
Pignatar0md <pignataromd@gmail.com>
2018-03-03 12:16:49 -0300

    quita staticas de pauses_helper

---
+++ application/helpers/config_helper.php
@@ -0,0 +1,12 @@
+<?
+set_time_limit(30);
+require "/var/lib/asterisk/agi-bin/phpagi.php";
+require_once "/var/lib/asterisk/agi-bin/phpagi-asmanager.php";
+define('IP_AMI', '');
+define('USER_AMI', '');
+define('PASS_AMI', '');
+define('IP_MySQL', '');
+define('USER_MySQL', '');
+define('PASS_MySQL', '');
+define('ID_PAUSA', );
+?>
--- application/helpers/pauses_helper.php
+++ application/helpers/pauses_helper.php
@@ -1,14 +1,6 @@
 <?php
-set_time_limit(30);
-require "/var/lib/asterisk/agi-bin/phpagi.php";
-require_once "/var/lib/asterisk/agi-bin/phpagi-asmanager.php";
-define('IP_AMI', '');
-define('USER_AMI', '');
-define('PASS_AMI', '');
-define('IP_MySQL', '');
-define('USER_MySQL', '');
-define('PASS_MySQL', '');
-define('ID_PAUSA', );
+
+include 'config_helper.php';
 
 function auto_pause($sipext) //********************************** Funcion Auto-Pause
 {
